2725fed168e929e9c39934bcba6de8e9e8efc40d
stevemolloy <sdmolloy@gmail.com>
2016-12-20 15:31:53 +0100

    Removed the utility functions to book cos they were dumb

--- web/views/book_view.ex
+++ web/views/book_view.ex
@@ -2,15 +2,15 @@ defmodule StoryTime.BookView do
   use StoryTime.Web, :view
   alias StoryTime.Book
 
-  def book_name(%Book{title: title}) do
-    title
-  end
-
-  def book_mainchar(%Book{mainChar: mainChar}) do
-    mainChar
-  end
-
-  def book_titlechar(%Book{titleChar: titleChar}) do
-    titleChar
-  end
+  # def book_name(%Book{title: title}) do
+  #   title
+  # end
+  #
+  # def book_mainchar(%Book{mainChar: mainChar}) do
+  #   mainChar
+  # end
+  # 
+  # def book_titlechar(%Book{titleChar: titleChar}) do
+  #   titleChar
+  # end
 end
